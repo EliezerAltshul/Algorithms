b1f857f662b37e5ff938b719a866ade86421a311
Eliezer Altshul <eli.altshul@gmail.com>
2021-01-31 13:20:50 -0500

    Added Selection Sort

--- src/ealtshul/SortArrays.java
+++ src/ealtshul/SortArrays.java
@@ -18,16 +18,39 @@ public class SortArrays {
 		System.out.println("Original Array: ");
 		for(int i : arr) System.out.print(i + ", ");
 		
+		int[] sorted;
+		/*The selection sort algorithm sorts an array by repeatedly 
+		 * finding the minimum element (considering ascending order) 
+		 * from unsorted part and putting it at the beginning. The 
+		 * algorithm maintains two subarrays in a given array.*/
+		sorted = selectionSort(arr);
+		
+		
 		//display sorted array;
-		int[] sorted = insertionSort(arr);
-		System.out.println("\nSorted Array: ");
-		for(int i : sorted) System.out.print(i + ", ");
+		sorted = insertionSort(arr);
+		
 	}
-	
+	public static int[] selectionSort(int[] arr) {
+		System.out.println("\nSelection Sort: ");
+		for(int i = 0; i<arr.length; i++) {
+			int minIndex = i;
+			for(int j = 0; j<arr.length-i; j++) {
+				if(arr[j+i] < arr[minIndex]) {
+					minIndex = j+i;
+				}
+			}
+			int temp = arr[i];
+			arr[i] = arr[minIndex];
+			arr[minIndex] = temp;
+			System.out.print(arr[i] + ", ");
+		}
+		return arr;
+	}
 	/**Insertion sort is a simple sorting algorithm that 
 	 * builds the final sorted array one item at a time.
 	**/
 	public static int[] insertionSort(int[] arr) {
+		System.out.println("\nInsertion Sort: ");
 		int key = 0;
 		int i = 0;
 	 	for(int j=1; j<arr.length; j++) {
@@ -38,7 +61,9 @@ public class SortArrays {
 				i--;
 			}
 			arr[i+1] = key;
+			System.out.print(arr[j-1] + ", ");
 		}
+	 	System.out.print(arr[arr.length-1] + ", ");
 		return arr;
 	}
 	
